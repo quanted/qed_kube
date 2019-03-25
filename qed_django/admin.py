851504dddd70222145760fa1b6ef3c3f09037d42
dbsmith88 <dbsmith@uga.edu>
2019-03-25 12:12:51 -0500

    removed qed_django directory

--- qed_django/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .hem_app.models import Category, ProductAssignment, Product
-
-admin.site.register(Category)
-admin.site.register(ProductAssignment)
-admin.site.register(Product)
