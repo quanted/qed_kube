af1f8cffcb6b8bb186ab3a8382a9d7bf217da3a9
dbsmith88 <dbsmith@uga.edu>
2020-02-05 07:50:52 -0600

    update webapps command

--- qed_tomcat/tomcat_startup.sh
+++ qed_tomcat/tomcat_startup.sh
@@ -3,6 +3,7 @@ set -e
 echo "Creating links for mounted volume directories"
 ln -s /tmp/tomcat/.chemaxon/licenses /home/tomcat/.chemaxon/licenses
 cp -rf /tmp/tomcat/tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
-ln -s /tmp/tomcat/webapps /usr/local/tomcat/webapps
+rm -rf /usr/local/tomcat/webapps
+ln -s /tmp/tomcat/webapps /usr/local/tomcat
 
 exec "$@"
