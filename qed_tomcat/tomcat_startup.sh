e8737d24a3b513b0de6158afaba1925d6c3ef3f3
Deron Smith <deron.brock@gmail.com>
2021-10-18 10:13:08 -0500

    Path updates

--- qed_tomcat/tomcat_startup.sh
+++ qed_tomcat/tomcat_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 echo "Creating links for mounted volume directories"
-ln -s /tmp/tomcat/.chemaxon/licenses /home/tomcat/.chemaxon/licenses
-cp -rf /tmp/tomcat/tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
-rm -rf /usr/local/tomcat/webapps
-ln -s /tmp/tomcat/webapps /usr/local/tomcat
+ln -s /usr/local/tomcat/.chemaxon/licenses /home/tomcat/.chemaxon/licenses
+cp -rf /usr/local/tomcat/tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
+# rm -rf /usr/local/tomcat/webapps
+# ln -s /tmp/tomcat/webapps /usr/local/tomcat
 
 exec "$@"
