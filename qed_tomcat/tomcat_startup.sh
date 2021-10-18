766be6aab843f22e9b60a78947f94c3ae2b839a6
Deron Smith <deron.brock@gmail.com>
2021-10-18 14:17:43 -0500

    added mkdir for conf

--- qed_tomcat/tomcat_startup.sh
+++ qed_tomcat/tomcat_startup.sh
@@ -2,6 +2,7 @@
 set -e
 echo "Creating links for mounted volume directories"
 ln -s /usr/local/tomcat/.chemaxon/licenses /home/tomcat/.chemaxon/licenses
+mkdir /usr/local/tomcat/conf/
 cp -rf /usr/local/tomcat/tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
 # rm -rf /usr/local/tomcat/webapps
 # ln -s /tmp/tomcat/webapps /usr/local/tomcat
